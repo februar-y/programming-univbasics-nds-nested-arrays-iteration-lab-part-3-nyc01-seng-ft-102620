33aae07548dafd2fce2c59d86ad5cf7d6438baf0
rational-architecture-1641 <jarmah241@gmail.com>
2020-10-01 13:55:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,13 +1,10 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-  
   row = 0
+  
   while row < src.count do
-    inner_row = 0
-    while inner_row < src[row].count
     
   end
-    row += 1
-  end
+
 end
